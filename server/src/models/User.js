a69e44b264590000f4df43bbfabcbb751d704e76
Prerana Bubane <preranabubane@gmail.com>
2026-07-30 16:11:31 +0530

    Initial Comment

---
+++ server/src/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true, minlength: 6 },
+  skillsToTeach: [{ type: String }],
+  skillsToLearn: [{ type: String }],
+  credits: { type: Number, default: 10 }, // starting credits for new users
+  reputationScore: { type: Number, default: 0 },
+}, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Method to compare entered password with hashed password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
